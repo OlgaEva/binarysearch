207063f0f90a39a3fa358613418fbb5118bf755f
OlgaEva <olga.rosas@gmail.com>
2020-06-24 13:37:48 -0400

    add numeronym.js

---
+++ numeronym.js
@@ -0,0 +1,22 @@
+/*
+Numeronym
+Given a string s with length greater than 2, 
+return a new string where the first and last 
+letter are the same, but the letters in between 
+are replaced by their length.
+
+Example 1
+Input
+
+s = "croneberry"
+Output
+
+"c8y"
+Example 2
+Input
+
+s = "extracurricular"
+Output
+
+"e13r"
+*/
